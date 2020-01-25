be42a16780076a9ce34245832b8da772b8a86fb1
diya <geethusivaraman@gmail.com>
2020-01-25 10:00:26 -0800

    gotsa merge

--- routes/usersAuthHelper.js
+++ routes/usersAuthHelper.js
@@ -6,6 +6,6 @@ module.exports = function (req, res, next) {
 	if (req.isAuthenticated()) {
 		return next();
 	}
-	req.flash("error_msg", "Not Authorized");
+	//req.flas("error_msg", "Not Authorized");
 	res.redirect("/users/login");
 };
--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-// eslint-disable-next-line no-undef
-=======
->>>>>>> 6c71d20967f1cc02d3a564d1fb0981a1a63f3fa2
+/* eslint-disable no-undef */
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
@@ -11,69 +8,12 @@ var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-<<<<<<< HEAD
-const session = require("express-session");
-const flash = require("connect-flash");
-=======
 console.log("Hi");
->>>>>>> 6c71d20967f1cc02d3a564d1fb0981a1a63f3fa2
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-<<<<<<< HEAD
-
-// Handlebars
-app.engine(
-    "handlebars",
-    exphbs({
-        defaultLayout: "main"
-    })
-);
-app.set("view engine", "handlebars");
-
-// Express-session middleware
-app.use(session({
-    secret: "wolves",
-    resave: true,
-    saveUninitialized: true,
-    // cookie: { secure: true }
-}));
-
-// Passport config middleware
-const passport = require("passport");
-require("./config/passport")(passport);
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Express-flash middleware
-app.use(flash());
-
-// Variables to be used across the app for flash messages
-// Web-page layout is set in partials/msg.handebars and
-// partials/errors to be displayed at top of a page
-app.use(function (req, res, next) {
-    res.locals.success_msg = req.flash("success_msg");
-    res.locals.error_msg = req.flash("error_msg");
-    res.locals.error = req.flash("error");
-    res.locals.user = req.user || null;
-    next();
-});
-
-
-// Routes for users
-// --> middleware should come before other routes
-const userSession = require("./routes/users");
-const userRoutes = require("./routes/userRoutes");
-app.use("/users", userSession);
-app.use("/users", userRoutes);
-
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
-=======
 console.log("After app.use statements");
 // Handlebars
 app.engine(
@@ -91,46 +31,24 @@ app.use(require("./routes/users.js"));
 console.log("After users");
 app.use(require("./routes/usersAuthHelper.js"));
 console.log("After usersAuthHelper");
->>>>>>> 6c71d20967f1cc02d3a564d1fb0981a1a63f3fa2
 
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
-<<<<<<< HEAD
-    syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function () {
-    // Load mock data
-    if (process.argv[2]) {
-        const loadData = require(process.argv[2]);
-        loadData();
-    }
-
-    app.listen(PORT, function () {
-        console.log(
-            "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-            PORT,
-            PORT
-        );
-    });
-=======
   syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
       PORT
     );
   });
->>>>>>> 6c71d20967f1cc02d3a564d1fb0981a1a63f3fa2
 });
 
 module.exports = app;
